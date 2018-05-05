d35229f2fcf6b5e486807a0304a98f28c71cdcc4
deni1688 <denisj1688@gmail.com>
2018-05-05 20:38:16 +0200

    api: initial post routes done

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,9 +5,48 @@ const passport = require("passport");
 
 // load User model
 const Post = require("../../models/Post");
+// load User model
+const Profile = require("../../models/Profile");
 
 // load validation for post inputs
 const validatePostInput = require("../../validation/post");
+
+/*
+ * @route   GET api/posts
+ * @desc    Get Posts
+ * @access  Public
+ */
+router.get("/", (req, res) => {
+  const errors = {};
+  Post.find({})
+    .sort({ date: -1 })
+    .then(posts => {
+      if(!posts){
+        errors.noPosts = "No posts found";  
+        return res.status(404).json(errors);
+      }
+      res.json(posts);
+    })
+    .catch(err => res.status(404).json({noPosts: "No posts found"}));
+});
+
+/*
+ * @route   GET api/posts/:id
+ * @desc    Get Post by id
+ * @access  Public
+ */
+router.get("/:id", (req, res) => {
+  const errors = {};
+  Post.findById(req.params.id)
+    .then(post => {
+      if(!post){
+        errors.noPost = "That post does not exist";  
+        return res.status(404).json(errors);
+      }
+      res.json(post);
+    })
+    .catch(err => res.status(404).json({noPost: "That post does not exist"}));
+});
 
 /*
  * @route   POST api/posts
@@ -32,10 +71,31 @@ router.post(
       user: req.user.id
     });
 
-    newPost.save()
-      .then(post => res.json(post));
-    
+    newPost.save().then(post => res.json(post));
   }
 );
+
+/*
+ * @route   DELETE api/posts/:id
+ * @desc    Get Post by id
+ * @access  Private
+ */
+router.delete("/:id", passport.authenticate("jwt", {session: false }), (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.user.id })
+    .then( profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+            // verify post owner
+            if(post.user.toString() !== req.user.id){
+              return res.status(401).json({notAuthorized: "User not authorized"})
+            }
+
+            post.remove()
+              .then(() => res.json({success: true}))
+        })
+        .catch(err => res.status(404).json({noPost: "Post not found" }))
+    })
+});
 
 module.exports = router;
--- validation/post.js
+++ validation/post.js
@@ -9,8 +9,8 @@ module.exports = validatePostInput = data => {
 
 
 
-  if (!Validator.isLength(data.text, {min: 10})) {
-    errors.text = "Text cannot be less than 10 characters";
+  if (!Validator.isLength(data.text, {min: 10, max: 300})) {
+    errors.text = "Text cannot be less than 10  or more than 300 characters";
   }
 
   if (Validator.isEmpty(data.text)) {
