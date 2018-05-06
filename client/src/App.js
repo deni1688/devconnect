44f89457cdac44d724efc1a8007a6852ef9d6d93
deni1688 <denisj1688@gmail.com>
2018-05-06 16:29:25 +0200

    client: added auth views

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
-import './App.scss';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Landing from './components/layout/Landing';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.scss";
+
+// components
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import PageNotFound from "./components/layout/PageNotFound";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+     <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+              <Route path="/login" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="**" component={PageNotFound} />
+          </Switch>
+          <Footer />
+        </div>
+     </Router>
     );
   }
 }
---
+++ client/src/components/layout/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-4 text-center">Page Not Found</h1>
+      </div>
+    </div>
+  )
+}
+
+export default PageNotFound;
